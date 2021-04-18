d426a4de277ba560cfdb63c2224f6a3d69ab05c8
HeyAero <aeroyts@icloud.com>
2021-04-18 22:06:54 +0100

    add all features to DrinkBox and update style with media queries to support mobile view

--- src/components/DrinkBox/index.js
+++ src/components/DrinkBox/index.js
@@ -42,33 +42,34 @@ const DrinkBox = () => {
   }
 
   return (
-    <section className="container drink">
+    <section className="container drink mt-5">
       { drink ? 
       <div className="row">
           <div className="col-md-4 drink-border-left">
             <h1 className="text-center">{drink.strDrink}</h1>
-            <img src={drink.strDrinkThumb} alt={drink.strDrink}></img>
-            <h5 className="text-center mt-2">Ingrediants:</h5>
+            <img src={drink.strDrinkThumb} alt={drink.strImageAttribution}></img>
+            <h5 className="text-center mt-2">Ingredients:</h5>
             <ul>
-              { drink.strIngredient1 ? <li>{drink.strIngredient1} {drink.strMeasure1}</li> : <></> }
-              { drink.strIngredient2 ? <li>{drink.strIngredient2} {drink.strMeasure2}</li> : <></> }
-              { drink.strIngredient3 ? <li>{drink.strIngredient3} {drink.strMeasure3}</li> : <></> }
-              { drink.strIngredient4 ? <li>{drink.strIngredient4} {drink.strMeasure4}</li> : <></> }
-              { drink.strIngredient5 ? <li>{drink.strIngredient5} {drink.strMeasure5}</li> : <></> }
-              { drink.strIngredient6 ? <li>{drink.strIngredient6} {drink.strMeasure6}</li> : <></> }
-              { drink.strIngredient7 ? <li>{drink.strIngredient7} {drink.strMeasure7}</li> : <></> }
-              { drink.strIngredient8 ? <li>{drink.strIngredient8} {drink.strMeasure8}</li> : <></> }
-              { drink.strIngredient9 ? <li>{drink.strIngredient9} {drink.strMeasure9}</li> : <></> }
-              { drink.strIngredient10 ? <li>{drink.strIngredient10} {drink.strMeasure10}</li> : <></> }
-              { drink.strIngredient11 ? <li>{drink.strIngredient11} {drink.strMeasure11}</li> : <></> }
-              { drink.strIngredient12 ? <li>{drink.strIngredient12} {drink.strMeasure12}</li> : <></> }
-              { drink.strIngredient13 ? <li>{drink.strIngredient13} {drink.strMeasure13}</li> : <></> }
-              { drink.strIngredient14 ? <li>{drink.strIngredient14} {drink.strMeasure14}</li> : <></> }
-              { drink.strIngredient15 ? <li>{drink.strIngredient15} {drink.strMeasure15}</li> : <></> }
+              { drink.strIngredient1 ? <li>{drink.strIngredient1} - {drink.strMeasure1}</li> : <></> }
+              { drink.strIngredient2 ? <li>{drink.strIngredient2} - {drink.strMeasure2}</li> : <></> }
+              { drink.strIngredient3 ? <li>{drink.strIngredient3} - {drink.strMeasure3}</li> : <></> }
+              { drink.strIngredient4 ? <li>{drink.strIngredient4} - {drink.strMeasure4}</li> : <></> }
+              { drink.strIngredient5 ? <li>{drink.strIngredient5} - {drink.strMeasure5}</li> : <></> }
+              { drink.strIngredient6 ? <li>{drink.strIngredient6} - {drink.strMeasure6}</li> : <></> }
+              { drink.strIngredient7 ? <li>{drink.strIngredient7} - {drink.strMeasure7}</li> : <></> }
+              { drink.strIngredient8 ? <li>{drink.strIngredient8} - {drink.strMeasure8}</li> : <></> }
+              { drink.strIngredient9 ? <li>{drink.strIngredient9} - {drink.strMeasure9}</li> : <></> }
+              { drink.strIngredient10 ? <li>{drink.strIngredient10} - {drink.strMeasure10}</li> : <></> }
+              { drink.strIngredient11 ? <li>{drink.strIngredient11} - {drink.strMeasure11}</li> : <></> }
+              { drink.strIngredient12 ? <li>{drink.strIngredient12} - {drink.strMeasure12}</li> : <></> }
+              { drink.strIngredient13 ? <li>{drink.strIngredient13} - {drink.strMeasure13}</li> : <></> }
+              { drink.strIngredient14 ? <li>{drink.strIngredient14} - {drink.strMeasure14}</li> : <></> }
+              { drink.strIngredient15 ? <li>{drink.strIngredient15} - {drink.strMeasure15}</li> : <></> }
             </ul>
           </div>
           <div className="col-md-8 drink-border-right">
-            
+            <h3 className="mt-2">Instructions:</h3>
+            <p>{ drink.strInstructions }</p>
           </div>
       </div>
         :
