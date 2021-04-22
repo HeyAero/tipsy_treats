e4f09ef2f3560ffbdb51c10bb6335096d64475fe
HeyAero <aeroyts@icloud.com>
2021-04-22 16:46:44 +0100

    add error message for when result not found on search results

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -4,15 +4,17 @@ const Results = ({results}) => {
   return (
     <div className="">
       { results ?
-        results.drinks.map(result => (
-          <div key={result.idDrink} className="result-box">
-            <h2>{result.strDrink}</h2>
-            <img src={result.strDrinkThumb} alt={result.strDrink}></img>
-            <hr/>
-            <a href={`/drink/${result.idDrink}`}>VIEW</a>
-          </div>
-        ))
-
+          results.drinks ?
+            results.drinks.map(result => (
+              <div key={result.idDrink} className="result-box">
+                <h2>{result.strDrink}</h2>
+                <img src={result.strDrinkThumb} alt={result.strDrink}></img>
+                <hr/>
+                <a href={`/drink/${result.idDrink}`}>VIEW</a>
+              </div>
+            ))
+          :
+          <h1 className="error-msg text-center mt-3">No results found.</h1>
         : <div></div>
       }
     </div>
